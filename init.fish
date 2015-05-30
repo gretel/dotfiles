8bc22e31f71f23d47452bc55215b8af1e1f35d76
Jorge Bucaran <jbucaran@me.com>
2015-05-31 07:02:38 +0900

    support for lib/completions

--- init.fish
+++ init.fish
@@ -1,9 +1,26 @@
-# Source any $WAHOO_CUSTOM/init.fish and autoload functions in:
-#   $WAHOO_CUSTOM/{theme/*,functions,pkg/*,*}
-#   $WAHOO_PATH/{theme/*,pkg/*}
+# SYNOPSIS
+#   Initialize Wahoo.
+#
+# ENV
+#   WAHOO_PATH    Set in ~/.config/fish/config.fish
+#   WAHOO_CUSTOM  Same as WAHOO_PATH. ~/.dotfiles by default.
+#   RESET_PATH    Original PATH. To preseve across Wahoo reloads.
+#
+# OVERVIEW
+#   Autoloads Wahoo's packages, themes and custom path (in that order),
+#   loading any <package>.fish files if available. If this succeeds,
+#   emits and init_<package> event.
+#
+#   Autoloads functions directory and sources init.fish under
+#   the custom path if available.
+
+set -q RESET_PATH
+  and set PATH $RESET_PATH
+  or set --export RESET_PATH $PATH
 
 set -l user_function_path $fish_function_path[1]
 set fish_function_path[1] $WAHOO_PATH/lib
+set fish_complete_path    $WAHOO_PATH/lib/completions
 
 set -l theme  {$WAHOO_PATH,$WAHOO_CUSTOM}/themes/(cat $HOME/.config/wahoo/theme)
 set -l paths  $WAHOO_PATH/pkg/*
@@ -20,5 +37,5 @@ for path in $paths $theme $custom
 end
 
 autoload $WAHOO_CUSTOM/functions
-autoload "$user_function_path"
+autoload $user_function_path
 source $WAHOO_CUSTOM/init.fish
