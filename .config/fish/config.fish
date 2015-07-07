39bf7bdbcf2e2020d8fee1c7c1d1892a4a9cb201
Tom Hensel <tom@jitter.eu>
2015-07-07 16:08:11 +0200

    installed wahoo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
+# https://github.com/bucaran/wahoo
 set -g WAHOO_PATH $HOME/.wahoo
 set -g WAHOO_CUSTOM $HOME/.dotfiles
 source $WAHOO_PATH/init.fish
 
-set -Ux DOMAIN (hostname -s)
-set -Ux HOSTNAME (hostname)
-set -gx UID (id -u $USER)
+# https://ansible.com/
+source $HOME/Sync/prjcts/ansible/hacking/env-setup.fish
+
+## user stuff
+#set -Ux DOMAIN (hostname -s)
+#set -Ux HOSTNAME (hostname)
+#set -gx UID (id -u $USER)
