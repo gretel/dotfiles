6bc6a21c85395780e59a6b2e808b2f92886a90cd
Tom Hensel <github@jitter.eu>
2015-07-12 04:09:58 +0200

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,9 @@
+set -g ANSIBLE_HOME $HOME/Sync/prjcts/ansible
+
 # https://github.com/bucaran/wahoo
 set -g WAHOO_PATH $HOME/.wahoo
 set -g WAHOO_CUSTOM $HOME/.dotfiles
 source $WAHOO_PATH/init.fish
-
-# https://ansible.com/
-source $HOME/Sync/prjcts/ansible/hacking/env-setup.fish
 
 ## user stuff
 #set -Ux DOMAIN (hostname -s)
