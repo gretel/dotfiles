8bac8678f802892d1f261ebfd0026c94c298865b
Tom Hensel <github@jitter.eu>
2015-07-18 17:39:24 +0200

    dont overwrite .direnvrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,8 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".direnvrc" \
+        -avh --no-perms . ~;
 #	source ~/.bash_profile;
 }
 
