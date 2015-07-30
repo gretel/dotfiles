681f54f9642f9eab88a84197677740817625977f
Tom Hensel <github@jitter.eu>
2015-07-30 12:11:41 +0200

    reflect reality and update py packages

--- bin/update.sh
+++ bin/update.sh
@@ -9,33 +9,31 @@
 
 echo "brew:"
 brew update; brew upgrade
-brew cleanup; brew prune
+brew cleanup; brew prune; brew linkapps
 
-echo "brew cask nightlies:"
-rm /Library/Caches/Homebrew/chromium-latest
-brew cask install chromium --force
+echo "brew cask:"
+#rm /Library/Caches/Homebrew/chromium-latest
+#brew cask install chromium --force
 brew cask cleanup
 
-echo "\nfish:"
-cd .oh-my-fish; git pull --recurse-submodules
-
 echo "\nrubygems:"
-brew link --force --overwrite ruby
+#brew link --force --overwrite ruby
 yes | gem update --system --quiet
 yes | gem update --quiet
 cd ~; bundle update
 
 echo "\npip:";
 #brew link python --force --overwrite
-pip install --upgrade setuptools
-pip install --upgrade pip
+pip install -U setuptools
+pip install -U pip
+pip list --outdated | sed 's/(.*//g' | xargs pip install -U
 
 echo "\nnpm:"
-npm update -g > /dev/null
+npm update -g # > /dev/null
 
-echo "\ntaskpaper:"
-~/bin/TaskPaperDaily.rb
+#echo "\ntaskpaper:"
+#~/bin/TaskPaperDaily.rb
 
-terminal-notifier -message 'updated yo' -title 'Toolset Update' -subtitle $0 -sound Morse
+terminal-notifier -message 'update done yo' -title 'toolchain' -subtitle $0 -sound Morse
 
 sync
