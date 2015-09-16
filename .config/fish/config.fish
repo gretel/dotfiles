87fb558f190b11cc82bfe540df1e2d09fc7241da
Tom Hensel <github@jitter.eu>
2015-09-16 21:03:59 +0200

    remove fishy

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-set -g PRJCTS_PHOME $HOME/Sync/prjcts
-set -g ANSIBLE_HOME $PRJCTS_PHOME/ansible
-
-# https://github.com/bucaran/wahoo
-set -g WAHOO_PATH $HOME/.wahoo
-set -g WAHOO_CUSTOM $HOME/.dotfiles
-source $WAHOO_PATH/init.fish
