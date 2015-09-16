7a95187af1661ac9481e7325c13894512df64578
Tom Hensel <github@jitter.eu>
2015-09-16 21:16:10 +0200

    add settings for thefuck

---
+++ .thefuck/settings.py
@@ -0,0 +1,9 @@
+# ~/.thefuck/settings.py: The Fuck settings file
+#
+# rules = ['sudo', 'no_command']
+exclude_rules = ['git_push', 'rm_dir']
+# require_confirmation = True
+# wait_command = 10
+# no_colors = False
+priority = {'sudo': 100, 'no_command': 9999}
+# debug = True
