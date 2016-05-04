c9eff3b2d643933c8030b42a57b503a8f4908d26
Tom Hensel <github@jitter.eu>
2016-05-05 01:47:21 +0200

    ensure xcode cli

--- bin/update.sh
+++ bin/update.sh
@@ -5,27 +5,15 @@ echo "brew:"
 #cd $(brew --prefix)
 #git fetch origin
 #git reset --hard origin/master
-#
-# cd $(brew --repository)
-# git reset --hard FETCH_HEAD
-# cd $(brew --repository)/Library
-# git clean -fd
-#
+cd $(brew --repository)
+git reset --hard FETCH_HEAD
+cd $(brew --repository)/Library
+git clean -fd
 cd ~
 brew update; brew upgrade
 #brew tap Homebrew/bundle;
 brew bundle
 brew cleanup; brew cask cleanup; brew prune; brew linkapps
-
-# TODO: abstraction
-for p in 2.7.11 3.5.1
-do
-  echo "\npip for ${p}:"
-  pyenv local $p
-  pip install -U setuptools
-  pip install -U pip
-  pip list --outdated | sed 's/(.*//g' | xargs pip install -U
-done
 
 echo "\nrubygems:"
 yes | gem update --system --quiet
@@ -35,17 +23,22 @@ echo "\nbundler:"
 gem install bundler
 bundle update
 
+echo "\npip:";
+pip install -U setuptools
+pip install -U pip
+pip list --outdated | sed 's/(.*//g' | xargs pip install -U
+pyenv rehash
+
 echo "\nnpm:"
 npm update -g
 
 echo "\nperu:"
-# TODO: abstraction
-pyenv local 3.5.1
+pyenv local 3.4.3
 peru -v sync
 
-#echo "\nsyncthing:"
-#launchctl unload /usr/local/opt/syncthing/homebrew.mxcl.syncthing.plist
-#launchctl load /usr/local/opt/syncthing/homebrew.mxcl.syncthing.plist
+echo "\nsyncthing:"
+launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.syncthing.plist
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.syncthing.plist
 
 terminal-notifier -message 'update done yo' -title 'toolchain' -subtitle $0 -sound Morse
 
