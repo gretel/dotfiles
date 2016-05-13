e32b0a6be33c98d93bc484a5dc21f022bdf994d9
Tom Hensel <github@jitter.eu>
2016-05-14 01:34:42 +0200

    migrate from chruby to fry

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,21 @@
 # $ brew update; brew install chruby chruby-fish direnv hub fasd fzf keychain pyenv thefuck
 
 # my personal ssh keys on the chain
-set ssh_keys $HOME/.ssh/svaha $HOME/.ssh/github $HOME/.ssh/stash
+set ssh_keys $HOME/.ssh/id_rsa $HOME/.ssh/github
+set gpg_keys 640F9BDD
 
 # https://github.com/yyuu/pyenv-virtualenv
 set -x PYENV_SHELL fish
-set -x PYENV_HOME $HOME/.pyenv
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PATH $PYENV_ROOT/bin;
 
 . (pyenv init - | psub)
 . (pyenv virtualenv-init - | psub)
 
-# chruby handles ruby
-. /usr/local/share/chruby/chruby.fish
-. /usr/local/share/chruby/auto.fish
+# # chruby handles ruby
+# . /usr/local/share/chruby/chruby.fish
+# . /usr/local/share/chruby/auto.fish
+source /usr/local/opt/fry/share/fry/fry.fish
 
 # direnv last so chruby and pyenv will have stuff set
 eval (direnv hook fish)
@@ -24,22 +27,23 @@ if status --is-interactive
   # fzf for history fuzzines
   . /usr/local/Cellar/fzf/**/shell/key-bindings.fish
 
-  # hub for 'git'
+  # hub for git
   eval (hub alias -s)
-
-  # keychain for 'ssh' and 'gnupg'
-  keychain --eval --quiet --quiet $ssh_keys >/dev/null
-  . $HOME/.keychain/(hostname)-fish
 
   # thefuck for the mess we type
   eval (thefuck --alias | tr '\n' ';')
 
-  # fasd autojumping to directories
+  # keychain for ssh and gpg2
+  eval (keychain --eval --dir $HOME/.keychain --inherit any-once --agents gpg,ssh $ssh_keys $gpg_keys)
+  set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
+
+  # fasd autojump to directories
   function -e fish_preexec _run_fasd
     fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
   end
 
   function j
+    # alias for fasd autojump
     cd (fasd -d -e 'printf %s' "$argv")
   end
 
@@ -49,8 +53,6 @@ if status --is-interactive
     bind \ct -M insert '__fzf_ctrl_t'
     bind \cr -M insert '__fzf_ctrl_r'
     bind \ec -M insert '__fzf_alt_c'
-
-    # TOOO add fasd bindings
   end
 
 end
