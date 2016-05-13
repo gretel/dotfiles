4d0f3499a7a97090a8a6fa8f77621fa54f70ed73
Tom Hensel <github@jitter.eu>
2016-05-13 02:31:40 +0200

    add PYTHONSTARTUP stuff

---
+++ .python/history.py
@@ -0,0 +1,42 @@
+# Initially stollen from github.com/coderholic/config
+# Most of these from http://sontek.net/tips-and-tricks-for-the-python-interpreter
+
+## useful modules
+import os
+import sys
+
+try:
+    import readline
+except ImportError:
+    pass
+else:
+    import rlcompleter
+    if 'libedit' in readline.__doc__:
+        readline.parse_and_bind("bind ^I rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
+
+    # Enable a History
+    HISTFILE="%s/.pyhistory" % os.environ["HOME"]
+
+    # Read the existing history if there is one
+    try:
+        readline.read_history_file(HISTFILE)
+    except:
+        pass
+
+    # Set maximum number of items that will be written to the history file
+    readline.set_history_length(500)
+
+    def savehist():
+        import readline
+        global HISTFILE
+        readline.write_history_file(HISTFILE)
+
+    import atexit
+    atexit.register(savehist)
+    del atexit
+finally:
+    del rlcompleter
+
+WELCOME=''
