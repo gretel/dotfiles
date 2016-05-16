4e86c862da08da4b80ba114962d9159b7f42a6c0
Tom Hensel <github@jitter.eu>
2016-05-16 20:25:14 +0200

    pyniceish

--- bin/update.sh
+++ bin/update.sh
@@ -1,60 +1,70 @@
 #!/bin/sh
 #
-echo "updating. please wait!\n"
+printf "updating. please wait!\n\n"
 
+### gpg
 gpg2 --refresh-keys --keyserver hkps://pgp.mit.edu
 
-echo "\nxcode:"
+### apple
+printf "\nappstore:\n"
+# mas install 443126292
+mas upgrade
+printf "\nxcode:\n"
 xcode-select --install
 
-echo "\nhomebrew:"
+printf "\nhomebrew:\n"
 # # repair
 # cd $(brew --repository)
 # git reset --hard FETCH_HEAD
 # cd $(brew --repository)/Library
 # git clean -fd
-
 brew analytics off
 # update
 brew update; brew upgrade
 # .Brewfile
-cd ~; brew tap Homebrew/bundle; brew bundle --global
+cd "$HOME" || exit
+brew tap Homebrew/bundle; brew bundle --global
 # cask
 brew cask cleanup
 # tidy
 brew cleanup; brew prune; brew linkapps
 # services
-echo "\n"; brew services list
+brew services clean; brew services list
 
-echo "\appstore:"
-# mas install 443126292
-mas upgrade
 
-echo "\nrubygems:"
+### ruby
+printf "\nrubygems:\n"
 yes | gem update --system --quiet
 yes | gem update --quiet
 
-echo "\nbundler:"
+printf "\nbundler:\n"
 gem install bundler --no-document
 bundle update
 
-echo "\nnpm:"
-npm update -g || npm install -g npm@latest
+### node.js
+printf "\nnpm:\n"
+npm update || npm install -g npm@latest
 
-# pip
-for p in 2.7.11 3.5.1
+pys="2.7.11 3.5.1"
+
+### python
+for p in $pys
 do
-  echo "\npip for ${p}:"
-  pyenv local $p
+  printf "\npip for %s:\n" "$p"
+  pyenv local "$p"
   pip install -q -U setuptools pip
   pip list --outdated | sed 's/(.*//g' | xargs pip install -U
+  case "$p" in
+    2.*.*)
+      pip install -U -r req_py2.txt
+      ;;
+    3.*.*)
+      pip install -U -r req_py3.txt
+      printf "\nperu:\n"
+      peru -v sync -f
+      ;;
+   esac
 done
 
-# peru
-echo "\nperu:"
-pyenv local 3.5.1
-pip install -U -r req_py3.txt
-peru -v sync -f
-
-# done
-terminal-notifier -message 'done dating up' -title 'update.sh' -subtitle $0 -sound Morse
+### done
+terminal-notifier -message 'done dating up' -title 'update.sh' -subtitle "$0" -sound Morse
