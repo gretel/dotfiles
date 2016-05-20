e0b51f6d1b471f1d1aa84a73f4eebd0a04f192bf
Tom Hensel <github@jitter.eu>
2016-05-20 06:37:45 +0200

    fix keychain direnv last

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,14 @@
-# osx-specific fish shell configuration https://gist.github.com/gretel/0bb5f77cdc54182c15dd
+# https://gist.github.com/gretel/0bb5f77cdc54182c15dd
 
 ### local configuration (not in git)
-source "$HOME/.config/fish/local.fish"
+source $HOME/.config/fish/local.fish
+
+### disable greeting
+set -e fish_greeting
 
 ### pyenv
 # source (pyenv init - | psub)
 # source (pyenv virtualenv-init - | psub)
-
-####
-eval (direnv hook fish)
-eval (direnv stdlib)
 
 if status --is-interactive
 
@@ -23,7 +22,12 @@ if status --is-interactive
   eval (thefuck --alias | tr '\n' ';')
 
   ### keychain
-  eval (keychain --eval --quick --dir $HOME/.keychain --inherit any --agents gpg,ssh $SSH_KEYS $GPG_KEYS)
-  set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
+  keychain --quick --quiet --timeout 3600 --inherit any --confhost --agents ssh,gpg $SSH_KEYS $GPG_KEYS
+  source $HOME/.keychain/(hostname)-fish
+  source $HOME/.keychain/(hostname)-fish-gpg
+  # set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
 
 end
+
+### direnv
+eval (direnv hook fish)
