3744e455cec492ebdd112ef5571a0d6c2b9a9445
Tom Hensel <github@jitter.eu>
2016-05-27 22:10:42 +0200

    sync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@
 ### disable greeting
 set -e fish_greeting
 
-
 ### basic
 set -x SHELL (which fish);
 set -x PLATFORM (uname);
@@ -13,6 +12,32 @@ set -x PREFIX /usr/local;
 set -x XDG_CACHE_HOME $HOME/.cache;
 
 if status --is-interactive
+
+    set -x RY_RUBIES $HOME/.rubies;
+
+    ### auth
+    set -x SSH_KEYS $HOME/.ssh/id_rsa $HOME/.ssh/github;
+    set -x GPG_KEYS '640F9BDD';
+
+    ### all platforms
+    set -x PAGER 'less';
+
+    set -x LC_ALL 'en_US.UTF-8';
+    set -x GCC_COLORS 1;
+    set -x CLICOLOR 1;
+    set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx;
+
+    if test "$PLATFORM" = 'Darwin'
+        # osx
+        set -x BROWSER 'open';
+        set -x EDITOR  'subl -n';
+        set -x VISUAL  'joe';
+
+        set -x GPG_TTY (which tty);
+    else
+        # all other
+        set -x EDITOR 'joe';
+    end
 
     if not test -d $HOME/.local/share/fish/generated_completions/
         echo "Updating completions..."
@@ -36,97 +61,81 @@ if status --is-interactive
     end
 
     ### hub
-    # eval (hub alias -s)
+    # eval (command hub alias -s)
 
     ### thefuck
-    thefuck --alias | tr '\n' ';' | source
+    command thefuck --alias | tr '\n' ';' | source
 
-    ### pyenv
-    source (pyenv init -|psub)
-    source (pyenv virtualenv-init -|psub)
+    # set -x PATH '/usr/local/Cellar/pyenv-virtualenv/HEAD/shims' $PATH;
+    set -x PYENV_VIRTUALENV_INIT 1;
+
+    function pyenv
+        set command $argv[1]
+        set -e argv[1]
+
+        switch "$command"
+            case rehash shell
+                source (pyenv "sh-$command" $argv | psub)
+
+            case \*
+                command pyenv "$command" $argv
+        end
+    end
 
     ### direnv
-    direnv hook fish | source
+    command direnv hook fish | source
 
     ### keychain
-    # eval (keychain --eval --inherit any --confhost --agents gpg,ssh $GPG_KEYS $SSH_KEYS)
-    keychain --nogui --eval --inherit any --confhost --agents ssh,gpg $SSH_KEYS $GPG_KEYS | source
+    # keychain --nogui --eval --inherit any --confhost --agents ssh,gpg $SSH_KEYS $GPG_KEYS | source
+    command keychain --nogui --eval --inherit any --confhost --agents ssh $SSH_KEYS | source
     # source $HOME/.keychain/(hostname)-fish
     # source $HOME/.keychain/(hostname)-fish-gpg
     # set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
 
     # FIXME: move, damnit
     function __setup_aliases_osx
-        alias "rl"   "rm -f .config/fish/fishd.*; source $HOME/.config/fish/config.fish; command fish -v"
+        alias "rl"   "rm .config/fish/fishd.* 2>/dev/null; source $HOME/.config/fish/config.fish; command fish -v"
 
         ### aliases
-        alias 'g'    'command git'
-        alias 'h'    'command hub'
-        alias 'gs'   'command gitsome'
-        alias 'sl'   'command sublime'
-        alias 'pe'   'command pyenv'
-        alias 'de'   'command direnv'
+        alias 'g'    'git'
+        alias 'h'    'hub'
+        alias 'gs'   'gitsome'
+        alias 'sl'   'sublime'
+        alias 'pe'   'pyenv'
+        alias 'de'   'direnv'
         alias 'j'    'z'
         alias 'jj'   'zz'
         alias 'prj'  'z prj'
 
         ### exa
-        alias 'ls'   'command exa'
-        alias 'l'    'command exa -a -lgmH'
+        alias 'ls'   'exa'
+        alias 'l'    'exa -a -lgmH'
         alias 'lg'   'l --git'
 
         alias 'la'   'l -@'
         alias 'll'   'l -h'
         alias 'llg'  'll --git'
 
-        alias 'l1'   'command exa -1 --group-directories-first'
+        alias 'l1'   'exa -1 --group-directories-first'
         alias 'la1'  'l1 -a'
 
-        alias 'le'   'command exa -a -lgH -s extension --group-directories-first'
+        alias 'le'   'exa -a -lgH -s extension --group-directories-first'
         alias 'leg'  'le --git'
 
-        alias 'lm'   'command exa -a -lghH -s modified -m'
+        alias 'lm'   'exa -a -lghH -s modified -m'
         alias 'lmg'  'lm --git'
 
-        alias 'lu'   'command exa -a -lghH -s modified -uU'
+        alias 'lu'   'exa -a -lghH -s modified -uU'
         alias 'lug'  'lu --git'
 
-        alias 'lt'   'command exa -T'
-        alias 'llt'  'command exa -a -lgHh -R -T'
+        alias 'lt'   'exa -T'
+        alias 'llt'  'exa -a -lgHh -R -T'
         alias 'tree' 'llt'
 
-        alias 'lr'   'command exa -a -lgHh -R -L 2'
-        alias 'lrg'  'command exa -a -lgHh -R -L 2 --git'
-        alias 'lrr'  "spin 'command exa -a -lgHh -R'"
+        alias 'lr'   'exa -a -lgHh -R -L 2'
+        alias 'lrg'  'exa -a -lgHh -R -L 2 --git'
+        alias 'lrr'  "spin 'exa -a -lgHh -R'"
     end
-
-    set -x RY_RUBIES $HOME/.rubies;
-
-    ### auth
-    set -x SSH_KEYS $HOME/.ssh/id_rsa $HOME/.ssh/github;
-    set -x GPG_KEYS '640F9BDD';
-
-    ### all platforms
-    set -x PAGER 'command less';
-
-    set -x LC_ALL 'en_US.UTF-8';
-    set -x GCC_COLORS 1;
-    set -x CLICOLOR 1;
-    set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx;
-
-    if test "$PLATFORM" = 'Darwin'
-        # osx
-        set -x BROWSER 'command open';
-        set -x EDITOR  'command subl -n';
-        set -x VISUAL  'command subl -n';
-
-        set -x GPG_TTY (which tty);
-        set -x HOMEBREW_GITHUB_API_TOKEN '0ee60b785c82f7554b15a19bcdaaa370effbe7c6';
-
-        __setup_aliases_osx
-    else
-        # all other
-        set -x EDITOR 'command joe';
-    end
+    __setup_aliases_osx
 
 end
