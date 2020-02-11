460392fba14af961e265ade8428228e2d16a9e67
gretel <code@jitter.eu>
2020-02-11 21:24:31 +0100

    renovate

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,20 +15,24 @@ set -x PREFIX /usr/local
 # freedesktop style cache location
 set -x XDG_CACHE_HOME $HOME/.cache
 
+# hail international
+set -x LC_ALL en_US.UTF-8
+
 # long process done
 set -U __done_min_cmd_duration 10000
 set -U __done_exclude 'git (?!push|pull)'
 
 if status --is-interactive
-    ### auth
-    set -x SSH_KEYS $HOME/.ssh/id_rsa $HOME/.ssh/github $HOME/.ssh/id_ed25519
-    #set -x GPG_KEYS '64236423'
+
+    ### update auto completions if not exist
+    if not test -d $HOME/.local/share/fish/generated_completions
+        fish_update_completions
+    end
 
     ### cosmetical
-    # set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
+    set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
     set -x CLICOLOR 1
     set -x GCC_COLORS 1
-    set -x LC_ALL en_US.UTF-8
 
     ### switch according to platform
     if test "$PLATFORM" = "Darwin" # osx
@@ -43,40 +47,31 @@ if status --is-interactive
         set -x EDITOR  'vi'
     end
 
-    ### shared Makefile
-    # TODO: revamp
-    alias 'mmake' 'make -f ~/Makefile'
+    ### workaround "for fish_update_completions"
+    set -x MANPATH /usr/share/man /usr/local/share/man $MANPATH
+
+    ### python virtualenv 'activate.fish' shall not oerride the prompt
+    set -x VIRTUAL_ENV_DISABLE_PROMPT yes
 
     ### tail file
     alias 'ff' 'less +F'
 
-    ### gnupg
-    if command --search gpg >/dev/null
-        # ensure gpg will have tty
-        set -x GPG_TTY (which tty)
-    end
-
-    ### kitty
-    if command --search kitty >/dev/null
-        command kitty + complete setup fish | source
-        alias 'd' 'kitty +kitten diff'
-    end
-    
     ### bat
     if command --search bat >/dev/null
         alias 'cat' 'bat'
+        alias 'less' 'bat'
         set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
         set -x BAT_CONFIG_PATH "$HOME/.batcfg"
+    end
+
+    ### curl
+    if test -d $PREFIX/opt/curl/bin
+        set -x PATH $PREFIX/opt/curl/bin $PATH
     end
 
     ### thefuck
     if command --search thefuck >/dev/null
         command thefuck --alias | tr '\n' ';' | source
-    end
-
-    ### keychain
-    if command --search keychain >/dev/null
-        command keychain --quiet --nogui --eval --inherit any-once --agents ssh --quick $SSH_KEYS | source
     end
 
     ### autojump
@@ -89,57 +84,76 @@ if status --is-interactive
         alias 'lw' 'command exa -g -G -a -s extension -F -U'
         alias 'll' 'command exa -g -l -a -s extension -F -U'
         alias 'lt' 'command exa -g -l -a -s extension -F -U -T -L 2'
+        alias 'ltd' 'command exa -g -l -a -s extension -F -U -T'
     end
 
     ### trash
     if command --search trash >/dev/null
         #set -l trash_cnt (string trim (trash -l | wc -l))
         #test "$trash_cnt" -gt 25; and echo "please consider emptying your trash of $trash_cnt items (type 'rme')."
-        alias 'rmm'  'command rm'
-        alias 'rm'   'command trash -v'
-        alias 'rml'  'command trash -l -v'
-        alias 'rme'  'command trash -e -v'
-        alias 'rms'  'command trash -s -v'
+        alias 'rmm' 'command rm'
+        alias 'rm'  'command trash -v'
+        alias 'rml' 'command trash -l -v'
+        alias 'rme' 'command trash -e -v'
+        alias 'rms' 'command trash -s -v'
     end
 
-    ### workaround "for fish_update_completions"
-    set -x MANPATH /usr/share/man /usr/local/share/man $MANPATH
+    # ### postgres
+    # if test -d $PREFIX/opt/postgresql/bin
+    #     set -x PATH $PREFIX/opt/postgresql/bin $PATH
+    # end
 
-    ### update auto completions if not exist
-    if not test -d $HOME/.local/share/fish/generated_completions
-        fish_update_completions
+    # ### rubygems
+    # if command --search ruby >/dev/null
+    #     set -x PATH $HOME/.gem/ruby/2.3.0/bin $PATH
+    # end
+
+    ### cargo
+    if command --search cargo >/dev/null
+        set -x PATH $HOME/.cargo/bin $PATH
     end
-end
 
-## curl
-if test -d $PREFIX/opt/curl/bin
-    set -x PATH $PREFIX/opt/curl/bin $PATH
-end
+    ### python 3.8 first
+    ### node (10)
+    set -x _NODE_BIN "/usr/local/opt/node@10/bin"
+    if test -d $_NODE_BIN
+        set -g fish_user_paths $_NODE_BIN $fish_user_paths
+    end
 
-# ### postgres
-# if test -d $PREFIX/opt/postgresql/bin
-#     set -x PATH $PREFIX/opt/postgresql/bin $PATH
-# end
+    set -x _PTYHON_BIN "/usr/local/opt/python@3.8/bin"
+    if test -d $_PTYHON_BIN
+        set -g fish_user_paths $_PTYHON_BIN $fish_user_paths
+    end
+ 
+    ### kitty
+    if command --search kitty >/dev/null
+        command kitty + complete setup fish | source
+        alias 'd' 'kitty +kitten diff'
+    end
+    
+    ### auth
+    set -x SSH_KEYS $HOME/.ssh/id_rsa $HOME/.ssh/github $HOME/.ssh/id_ed25519
+    #set -x GPG_KEYS '64236423'
 
-# ### rubygems
-# if command --search ruby >/dev/null
-#     set -x PATH $HOME/.gem/ruby/2.3.0/bin $PATH
-# end
+    ### gnupg
+    if command --search gpg >/dev/null
+        # ensure gpg will have tty
+        set -x GPG_TTY (which tty)
+    end
 
-### node (10)
-set -x PATH "/usr/local/opt/node@10/bin" $PATH
+    ### keychain
+    if command --search keychain >/dev/null
+        command keychain --quiet --nogui --eval --inherit any-once --agents ssh --quick $SSH_KEYS | source
+    end
 
-### cargo
-if command --search cargo >/dev/null
-    set -x PATH $HOME/.cargo/bin $PATH
-end
+    ### lastly, direnv
+    if command --search direnv >/dev/null
+        command direnv hook fish | source
+    end
 
-### lastly, direnv
-if command --search direnv >/dev/null
-    command direnv hook fish | source
-end
+    function fish_prompt
+        set duration (math -s6 "$CMD_DURATION / 1000")
+        eval "powerline-go -shell bare -duration $duration -error $status -cwd-max-depth 3 -max-width 50 -modules duration,venv,ssh,cwd,git,jobs,exit,root"
+    end
 
-function fish_prompt
-    set duration (math -s6 "$CMD_DURATION / 1000")
-    eval "$HOME/.go/bin/powerline-go -shell bare -duration $duration -error $status -cwd-max-depth 3 -max-width 50 -modules duration,docker,venv,ssh,cwd,git,jobs,exit,root"
 end
