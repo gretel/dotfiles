e5c501f96e2d442de6e5dcb9761040e12a93605c
gretel <code@jitter.eu>
2025-05-15 21:16:48 +0200

    renovate and extend config for favourite wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
-local config = wezterm.config_builder()
 local act = wezterm.action
+
+local config = wezterm.config_builder()
+-- config.debug_key_events = true
 
 config.default_prog = { '/opt/homebrew/bin/elvish' }
 
@@ -15,8 +14,8 @@ config.automatically_reload_config = true
 config.enable_scroll_bar = true
 config.hide_mouse_cursor_when_typing = false
 config.hide_tab_bar_if_only_one_tab = true
-config.macos_window_background_blur = 18
 config.pane_focus_follows_mouse = true
+config.scrollback_lines = 100000
 config.show_close_tab_button_in_tabs = false
 config.switch_to_last_active_tab_when_closing_tab = true
 config.tab_max_width = 3
@@ -26,9 +25,28 @@ config.window_background_opacity = 0.70
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'RESIZE'
 
+config.macos_fullscreen_extend_behind_notch = true
+config.macos_window_background_blur = 18
+
+config.skip_close_confirmation_for_processes_named = {
+  'elvish',
+}
+
 config.unix_domains = {
   {
     name = 'unix',
+  },
+}
+
+config.window_frame = {
+  font_size = 13.0,
+  active_titlebar_bg = '#404040',
+  inactive_titlebar_bg = '#202020',
+}
+
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = '#ee0099',
   },
 }
 
@@ -40,21 +58,22 @@ config.window_padding = {
 }
 
 config.mouse_bindings = {
-  -- Open URLs with Ctrl+Click
+  -- Open hyperlink
   {
     event = { Up = { streak = 1, button = 'Left' } },
     mods = 'CTRL',
     action = act.OpenLinkAtMouseCursor,
   },
+  -- Select on click
   {
     event = { Up = { streak = 1, button = 'Left' } },
     mods = 'NONE',
-    action = act.CompleteSelection 'ClipboardAndPrimarySelection'
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
   },
   {
     event = { Down = { streak = 1, button = 'Left' } },
     mods = 'CMD',
-    action = act.Nop
+    action = act.Nop,
   }
 }
 
@@ -65,31 +84,63 @@ config.leader = {
 }
 
 config.keys = {
+  -- Clear
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  },
+  -- New tab
+  {
+    key = 't',
+    mods = 'LEADER',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  },
+  -- Close tab
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = act.CloseCurrentTab { confirm = false },
+  },
+  -- Close pane
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  -- Toggle fullscreen
+  {
+    key = ' ',
+    mods = 'SHIFT|ALT',
+    action = act.ToggleFullScreen,
+  },
+  -- Show palette
   {
     key = 'p',
-    mods = 'SHIFT|CMD',
+    mods = 'LEADER',
     action = act.ActivateCommandPalette,
   },
+  -- Zoom pane
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = act.TogglePaneZoomState,
+  },
+  -- Copy mode
   {
     key = ']',
     mods = 'LEADER',
     action = act.ActivateCopyMode,
   },
-  -- Turn off the default CMD-m Hide action, allowing CMD-m to
-  -- be potentially recognized and handled by the tab
+  -- Quick select mode
   {
     key = '[',
     mods = 'LEADER',
     action = act.QuickSelect,
   },
-  {
-    key = '=',
-    mods = 'LEADER',
-    action = act.TogglePaneZoomState,
-  },
   -- Vertical split
   {
-    key = '.',
+    key = '/',
     mods = 'LEADER',
     action = act.SplitPane {
       direction = 'Right',
@@ -98,13 +149,45 @@ config.keys = {
   },
   -- Horizontal split
   {
-    key = '/',
+    key = '.',
     mods = 'LEADER',
     action = act.SplitPane {
       direction = 'Down',
       size = { Percent = 50 },
     },
-  }
+  },
+  -- Activate panes
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  -- Rotate panes
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.RotatePanes 'CounterClockwise',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.RotatePanes 'Clockwise',
+  },
 }
 
 -- and finally, return the configuration to wezterm
